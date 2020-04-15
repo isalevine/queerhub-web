0273fdd22667bcd6adb5f31693a6da57d8d83bc3
isalevine <isa.loves.coding@gmail.com>
2020-04-15 16:13:23 -0700

    added users_controller and create/destroy routes

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,9 @@
+class V1::UsersController < ApplicationController
+  def create
+    puts "hello from create!"
+  end
+
+  def destroy
+    puts "hello from destroy!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  # namespace :v1 do
-  #   get 'homepage/index'
-  # end
   root 'v1/homepage#index'
+  scope :v1 do
+    get 'users/create', to: 'v1/users#create'
+    get 'users/destroy', to: 'v1/users#destroy'
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
